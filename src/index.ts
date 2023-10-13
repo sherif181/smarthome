76e8bd74ed91f8b903f486a730e18bb82f734410
sherif <sherif@r14.roburnet.sk>
2023-10-13 23:29:23 +0200

    initial structure

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { expect } from "chai";
+
+console.log(1232)
+//alerimport { expect } from "chait(111)al)
---
+++ test/test.ts
@@ -0,0 +1,17 @@
+import { expect } from "chai";
+function test():int{
+    return 1
+}
+describe("Color Code Converter", function() {
+  describe("RGB to Hex conversion", function() {
+    it("converts the basic colors", function() {
+        expect(test()).to.equal(1);
+    });
+  });
+});
+
+
+
+
+    
+   
